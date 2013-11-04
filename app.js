b37faaa06de2764b65606fbbd45d8dca12240767
mahesh <mahesh.hebballi@gmail.com>
2013-11-04 05:33:33 +0530

    regex

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ var request = require('request');
 MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
 	if(err) throw err;
 
-	request('http://www.reddit.com/r/technology/.json', function(error, response, body){
-		if(!error && response.statusCode == 200) {
-			var obj = JSON.parse(body);
-			var stories = obj.data.children.map(function(story){return story.data;});
+	// request('http://www.reddit.com/r/technology/.json', function(error, response, body){
+	// 	if(!error && response.statusCode == 200) {
+	// 		var obj = JSON.parse(body);
+	// 		var stories = obj.data.children.map(function(story){return story.data;});
 
-			db.collection('reddit').insert(stories, function(err, data){
-				if(err) throw err;
-				console.dir(data);
-				db.close();
-			});
+	// 		db.collection('reddit').insert(stories, function(err, data){
+	// 			if(err) throw err;
+	// 			console.dir(data);
+	// 			db.close();
+	// 		});
+	// 	}
+	// });
+
+	var query = {'title':{'$regex':'Euro'}};
+	var projection = {'title':1, '_id':0};
+
+	db.collection('reddit').find(query, projection).each(function(err, doc){
+		if(err) throw err;
+		if(doc==null){
+			return db.close();
 		}
+		console.dir(doc.title);
 	});
+
 });
