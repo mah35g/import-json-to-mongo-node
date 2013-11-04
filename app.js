a95dec06cc80ac81f167ddb9ac103868f894e135
mahesh <mahesh.hebballi@gmail.com>
2013-11-04 06:02:20 +0530

    Sort, Skip, and Limit

--- app.js
+++ app.js
@@ -17,15 +17,36 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db){
 	// 	}
 	// });
 
-	var query = {'title':{'$regex':'Euro'}};
-	var projection = {'title':1, '_id':0};
+	// var query = {'title':{'$regex':'Euro'}};
+	// var projection = {'title':1, '_id':0};
 
-	db.collection('reddit').find(query, projection).each(function(err, doc){
+	// db.collection('reddit').find(query, projection).each(function(err, doc){
+	// 	if(err) throw err;
+	// 	if(doc==null){
+	// 		return db.close();
+	// 	}
+	// 	console.dir(doc.title);
+	// });
+
+
+	var grades = db.collection('grades');
+	//var cursor = grades.find({});
+	// No matter how you write the order of the following, 
+	// driver adjusts it to Sort, Skip, and Limit
+	//cursor.sort('grade', 1);
+	// cursor.sort([['grade',1], ['student',-1]])
+	// cursor.skip(1);
+	// cursor.limit(6);
+
+	var options = {'skip':1, 'limit':4, 'sort':[['grade',1],['student',-1]]};
+	var cursor = grades.find({}, {}, options); // find(query, project, options)
+
+	cursor.each(function(err,doc){
 		if(err) throw err;
 		if(doc==null){
-			return db.close();
+			db.close();
 		}
-		console.dir(doc.title);
+		console.dir(doc);
 	});
 
 });
